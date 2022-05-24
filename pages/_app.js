a5e1d2eabd12631afe25bae8f275ab5ae3b3ac51
Matthew Pierce <lesbianhaircut@outlook.com>
2022-05-24 19:08:43 -0400

    c

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,21 @@
 import "../styles/globals.scss";
 import Layout from "../components/Layout";
+import { NextSeo } from "next-seo";
+
 function MyApp({ Component, pageProps }) {
+  const SEO = {
+    title: "Primal Enjoyer",
+    description: "Find Aajonus Vonderplanitz articles through a search bar",
+    keywords: ["Aajonus Vonderplanitz", " raw primal", "raw meat"],
+    icon: "/favicon.ico",
+  };
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <NextSeo {...SEO} />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
 
