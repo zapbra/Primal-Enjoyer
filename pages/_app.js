4f785be47191d01bd8672821b5bdb8b1e1851e03
Matthew Pierce <matt-pierce@hotmail.com>
2022-05-24 06:45:30 -0400

    c

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,8 @@
 import "../styles/globals.scss";
+import { usePanelbear } from "@panelbear/panelbear-nextjs";
 import Layout from "../components/Layout";
 function MyApp({ Component, pageProps }) {
+  usePanelbear(process.env.SITE_ID);
   return (
     <Layout>
       <Component {...pageProps} />
