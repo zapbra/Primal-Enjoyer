a11d490990ebf6b5a659c53869712b3bd637a8d3
Matthew Pierce <lesbianhaircut@outlook.com>
2022-05-24 18:37:19 -0400

    c

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,9 @@
 import "../styles/globals.scss";
+import { usePanelbear } from "@panelbear/panelbear-nextjs";
 import Layout from "../components/Layout";
+
 function MyApp({ Component, pageProps }) {
+  usePanelbear;
   return (
     <Layout>
       <Component {...pageProps} />
