8a46f884a9bb4c018936658deab4657935d89c1a
Matthew Pierce <lesbianhaircut@outlook.com>
2022-06-16 18:03:09 -0400

    c

--- Functions/index.js
+++ Functions/index.js
@@ -31,7 +31,7 @@ const GetRelatedArticles = (data, preTags) => {
 };
 
 const FakeFunction = () => {
-  console.log("Im Fake! as fuck lol34dfff");
+  console.log("Im Fake! as fuck lol34dffdf");
 };
 
 export default GetRelatedArticles;
