e4628ef20c146e728a3b1e7ac48c2f4e817d6e7a
Matthew Pierce <matt-pierce@hotmail.com>
2022-07-08 16:35:52 -0400

    c

--- components/introduction/sectionFour/index.js
+++ components/introduction/sectionFour/index.js
@@ -2,12 +2,15 @@ import React from "react";
 import styled from "styled-components";
 import COLORS from "../../../Data/colors";
 
-const Header = styled.div``;
+const Header = styled.div`
+  background-color: ${(props) => props.colors.ultraLightBlue};
+  padding: 2rem 0;
+`;
 
 const index = () => {
   return (
     <>
-      <Header>
+      <Header colors={COLORS}>
         <h2>How Can You Start?</h2>
       </Header>
       <div>words</div>
