5877973bba661ee19c78ca1b4b9894ee9e8a5039
Matthew Pierce <lesbianhaircut@outlook.com>
2024-04-29 11:50:48 -0400

    x

--- src/app/Category/components/TextPreview.js
+++ src/app/Category/components/TextPreview.js
@@ -1,31 +1,21 @@
 import Link from "next/link";
-import { useState } from "react";
-import styled from "styled-components";
-import COLORS from "../../../../data/colors";
-import { UpperCase } from "../../../../utils/Functions";
+import {useState} from "react";
+import {UpperCase} from "../../../../utils/Functions";
 
-const Cont = styled.div`
-  max-width: 1016px;
-  margin: 0 auto;
 
-  padding: 16px;
-  @media only screen and (max-width: 400px) {
-    padding: 8px;
-  }
-`;
-const TextPreview = ({ categories }) => {
-  const [categoryLines, setCategoryLines] = useState(
-    categories.map((category, index) => {
-      return (
-        <Link key={index} href={`/Category/${category.title}`}>
-          <p className="inline-block mar-right-8">
-            {UpperCase(category.title)},{" "}
-          </p>
-        </Link>
-      );
-    })
-  );
-  return <Cont colors={COLORS}>{categoryLines}</Cont>;
+const TextPreview = ({categories}) => {
+    const [categoryLines, setCategoryLines] = useState(
+        categories.map((category, index) => {
+            return (
+                <Link key={index} href={`/Category/${category.title}`}>
+                    <p className="inline-block mar-right-8">
+                        {UpperCase(category.title)},{" "}
+                    </p>
+                </Link>
+            );
+        })
+    );
+    return <div>{categoryLines}</div>;
 };
 
 export default TextPreview;
