ab602cc26c96cc948ecbc008a0480103b90d4e7d
Matthew Pierce <lesbianhaircut@outlook.com>
2024-04-29 11:30:30 -0400

    add

--- src/app/Category/[slug]/Render.js
+++ src/app/Category/[slug]/Render.js
@@ -1,11 +1,10 @@
 import COLORS from "../../../../data/colors";
-import ArticleSection from "../components/ArticleSection";
 import {UpperCase} from "../../../../utils/Functions";
 
 
 const Render = ({sortedArticles, aajonusCatagory}) => {
     return (
-        <div colors={COLORS}>
+        <div>
             <div className="title-spec sm-spacer">
                 <h1 className="text-shadow">{UpperCase(aajonusCatagory.title)}</h1>
             </div>
